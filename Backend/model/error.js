4ddb818b422beacf3ac9dbe0943973351642f4d6
Anushka Rangari <83566191+anushkaDev9@users.noreply.github.com>
2024-01-30 11:32:42 -0800

    Delete Backend directory

--- Backend/model/error.js
+++
@@ -1,7 +0,0 @@
-class HttpError extends Error {
-    constructor(message, code) {
-      super(message); // Add a "message" property
-      this.code = code; // Adds a "code" property
-    }
-  }
-  module.exports = HttpError;
--- Backend/routes/UserRoutes.js
+++
@@ -1,8 +0,0 @@
-const express=require('express');
-const { check} = require('express-validator');
-const router=express.Router();
-const userController=require('../controllers/UserController');
-router.get('/',userController.getUsers);
-router.post('/signup',userController.signUp);
-router.post('/login',userController.login);
-module.exports=router;
